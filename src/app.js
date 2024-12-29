8f24678ba7f9f8d00e7fe7bbcb4393bcfe8ec55a
Sandipkushwaha20 <sandipkushwaha2437@gmail.com>
2024-12-30 01:24:42 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -2,21 +2,17 @@ const express = require("express");
 const { adminAuth, userAuth } = require("./middlewares/auth");
 const app = express();
 
-app.use("/admin", adminAuth);
+app.get("/admin/data", (req , res) =>{
+    throw new Error("Error");
+    res.send("data send")
+});
 
-app.get("/admin/getData", (req, res) =>{ 
-    res.send("Data send");
-})
-app.get("/admin/getAllCourses", (req , res) =>{
-    res.send("Courses data send")
+app.use("/",(err , req , res , next) =>{
+    if(err){
+        res.status(500).send("Something wend wrong.")
+    }
 })
 
-app.get("/user/data", userAuth, (req , res) =>{
-    res.send("User data send") 
-})
-app.put("/user/login" , (req , res) =>{
-    res.send("User logged in")
-})
 app.listen(3000, ()=>{
     console.log("App is listning at port no 3000");
 })
