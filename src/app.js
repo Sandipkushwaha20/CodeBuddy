0b24a20e51cc4764c2926b7ff6117873aed11407
Sandipkushwaha20 <sandipkushwaha2437@gmail.com>
2024-12-30 20:55:32 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -2,9 +2,26 @@ const express = require("express");
 
 const { adminAuth, userAuth } = require("./middlewares/auth");
 const { connectDB } = require("./config/database");
+const User = require("./models/user");
 const app = express();
 
+app.post("/signup", async(req , res)=>{
+    const user = new User({
+        firstName: "Radhe Radhe",
+        lastName: "Ram Ram",
+        emailId: "radhe@gmail.com",
+        password: "radhe@1233"
+    });
 
+    try{
+        await user.save();
+        res.send("User added successfully.");
+    }catch(err){
+        res.send("User not added.");
+    }
+})
+
+//Most of the time mongoose functions return a promise so use async and await
 connectDB().then(() =>{
     console.log("DB connected successfully.");
     app.listen(3000,() =>{
