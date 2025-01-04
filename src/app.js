2fa13d12ac391652f3f74b6b7064e458cf2e80b2
Sandipkushwaha20 <sandipkushwaha2437@gmail.com>
2025-01-04 17:06:59 +0530

    user authentication added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
-const { adminAuth, userAuth } = require("./middlewares/auth");
+const validator = require("validator")
+const {userAuth } = require("./middlewares/auth");
 const { connectDB } = require("./config/database");
 const User = require("./models/user");
 const {validateSignUpData, validateloginData} = require("./utils/validation");
@@ -12,6 +12,7 @@ const app = express();
 //! Middleware for parsing data from JSON formate
 app.use(express.json()); 
 app.use(cookieParser());
+
 //SignUp API
 app.post("/signup", async(req , res)=>{
     try{
@@ -50,13 +51,12 @@ app.post("/login", async(req , res)=>{
         validateloginData(req);
         const {emailId, password} = req.body;
         const user = await User.findOne({emailId}); // findOne() will always return an object while find() will always return an array
-        // console.log("User data: ", user);
+        
         if(!user){
             return res.status(401).send("You are not registered with this email. Please try to create a account.")
         }
-        // console.log("Stored Password: ", user[0]?.password);
+        
         const isPasswordValid = await bcrypt.compare(password, user?.password);
-        // console.log("Pass: ",isPasswordValid);
         if(!isPasswordValid){
             return res.status(401).send("Please enter correct password.")
         }
@@ -65,128 +65,35 @@ app.post("/login", async(req , res)=>{
         const token = jwt.sign({
                 _id: user._id
             },
-            "secret",{expiresIn: 60}
+            "secret",{expiresIn: '7d'}
         );
-        // console.log(token);
+        
         //Add the token to cookie and send res back to the user
-
-        // const token = "sdklsssdsidseu399--r=ueee8ee8eeueididjdjdj";
-        res.cookie("token", token);
+        res.cookie("token", token, {expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), httpOnly: true}); //Expire in 7 days
         
         return res.status(200).send("User logged in successfully.")
     }catch(err){
         return res.status(500).send("Unable to login" + err);
     }
 })
-
-                                      
+                           
 //Profile API
-app.get("/profile", async(req , res)=>{
+app.get("/profile", userAuth, async(req , res)=>{
     try{
-        const {userId} = req.body;
-        const {token} = req.cookies;
-        
-        if(!userId){
-            throw new Error("User id should be present.");
-        }
-        const user = await User.findById(userId);
-        if(!user){ 
-            throw new Error("User not found");
-        }
-
-        const decodedMessage = jwt.verify(token, "secret");
-        if(!decodedMessage){
-            throw new Error("Token invalid.");
-        }
-        user.password = undefined;
-        return res.status(200).send({user});
+        const user = req.user;
+        return res.status(200).send(user);
     }catch(err){
         return res.status(500).send("Error: " + err);
     }
     
 })
 
-//Feed API - GET /feed - get all the data from the DB
-app.get("/feed", async (req, res)=>{
-    const {emailId} = req.body;
-    // console.log(userEmail);
-    
+app.post("/sendConnectionReq", userAuth, async(req , res) =>{
     try{
-        // const user =  await User.findOne({emailId: `${userEmail}`}).sort({ createdAt: 1 });
-        const user =  await User.findOne({emailId});
-        
-        console.log(user);
-        if(user.length === 0){
-            res.status(404).send(`User is not find with this ${emailId} id.`);
-        }else res.status(200).send("User data");
+        const user = req.user;
+        return res.status(200).send(user.firstName + " send connection request successfully.");
     }catch(err){
-        res.status(500).send("Unable to find user data");
-        console.log("user not find");
-    }
-})
-                                
-
-//delete a user
-app.delete("/user/delete", async (req , res)=>{
-    const userEmail = req.body.emailId;
-    try{
-        const user = await User.findOneAndDelete(
-            {emailId: userEmail}
-        );
-        console.log("Deleted Data of User ",user);
-        res.send("User deleted successfully.");
-    }catch(err){
-        // console.err(err);
-        res.status(500).send("Unable to delete user.");
-        console.log("user conn't be deleted:  ", err);
-    }
-})
-
-//findOneAndUpdate user
-app.patch("/user/update", async (req , res)=>{
-    const {emailId, newEmail} = req.body;
-    console.log(emailId," === ", newEmail);
-    try{
-        const user = await User.findOneAndUpdate(
-        {emailId, emailId},
-        {emailId: `${newEmail}`}
-    );
-        console.log("Updated Data of User ",user);
-        res.send(user);
-    }catch(err){
-        // console.err(err);
-        res.status(500).send("Unable to updated user.");
-        console.log("user conn't be updated:  ", err);
-    }
-}) 
-
-//findByIdAndUpdate user
-app.patch("/user/:id", async (req , res) =>{
-    const userId = req.params?.id;
-    console.log(userId);
-    const data = req.body;
-    // console.log(data,"  ,  ");
-
-    try{
-        const Allowed_Updates = ["firstName","lastName","age","gender","about","skills"];
-        const isUpdateAllowed = Object.keys(data).every((k) =>{
-            // console.log(k,", ");
-            return Allowed_Updates.includes(k);
-        });
-        if(!isUpdateAllowed){
-            throw new Error("Update is not allowed");
-        }else{
-            const user = await User.findByIdAndUpdate(userId , data, {
-                returnDocument:'after',
-                runValidators: true,
-            });
-            console.log(user);
-            res.send(user);
-        }
-        
-    }catch(err){
-        console.log("User is unable to update by ID.")
-        res.status(500).send("User is unable to update by Id." + err);
+        return res.status(500).send("Unable to send connection request. Please try again!");
     }
 })
 
@@ -199,3 +106,79 @@ connectDB().then(() =>{
 }).catch(() =>{
     console.log("DB is not connected.")
 })  
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Feed API - GET /feed - get all the data from the DB
+// app.get("/feed", userAuth, async (req, res)=>{
+//     try{
+//         // const user =  await User.findOne({emailId: `${userEmail}`}).sort({ createdAt: 1 });
+//         const {emailId} = req.body;
+//         const user =  await User.findOne({emailId});
+        
+//         console.log(user);
+//         if(!user){
+//             res.status(404).send(`User is not find with this ${emailId} id.`);
+//         }else res.status(200).send("User data: " + user);
+//     }catch(err){
+//         res.status(500).send("Unable to find user data");
+//         console.log("user not find");
+//     }
+// })
+                                
+
+// //delete a user
+// app.delete("/user/delete", userAuth, async (req , res)=>{
+//     try{
+//         const user = await User.deleteOne(
+//             req.user._id
+//         );
+//         console.log("Deleted Data of User ",user);
+//         res.status(204).send("User deleted successfully.");
+//     }catch(err){
+//         // console.err(err);
+//         res.status(500).send("Error from deleteAPI:  Unable to delete user." + err);
+//         console.log("user conn't be deleted:  ", err);
+//     }
+// })
+
+// //findByIdAndUpdate user
+// app.patch("/user/UpdateById", userAuth, async (req , res) =>{
+//     // console.log(data,"  ,  ");
+    
+//     try{
+//         const userId = req.user?.id;
+//         // console.log(userId);
+//         const data = req.body;
+//         const Allowed_Updates = ["firstName","lastName","age","gender","about","skills"];
+//         const isUpdateAllowed = Object.keys(data).every((k) =>{
+//             // console.log(k,", ");
+//             return Allowed_Updates.includes(k);
+//         });
+
+//         if(!isUpdateAllowed){
+//             throw new Error("Update is not allowed");
+//         }else{
+//             const user = await User.findByIdAndUpdate(userId , data, {
+//                 returnDocument:'after',
+//                 runValidators: true,
+//             });
+//             // console.log(user);
+//             res.send(user);
+//         }
+        
+//     }catch(err){
+//         console.log("User is unable to update by ID.")
+//         res.status(500).send("User is unable to update by Id." + err);
+//     }
+// })
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,21 +1,35 @@
-const adminAuth = (req , res , next) =>{
-    console.log("Admin auth checked.")
-    const token = "sksksk";
-    if(token !== "sksksk"){
-        res.send("Admin is unAuthorised");
-    }else{
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const userAuth = async (req , res , next) =>{
+    try{
+        //read the token
+        const {token} = req.cookies;
+        console.log("User Auth Working...");
+
+        if(!token){
+            return res.status(404).send("Error from userAuth Middleware:  Token is not present. Please try to login first.");
+        }
+        const decodedObj = jwt.verify(token , "secret");
+        if(!decodedObj){
+            return res.status(404).send("Token invalid.");
+        }
+        const userId = decodedObj._id;
+        // console.log("ID: ", userId);
+
+        const user = await User.findById({_id: userId});
+        // console.log("USER: ", user);
+        if(!user){
+            return res.status(404).send("Error from userAuth Middleware:  User is not found. Please try again....");
+        }
+
+        req.user = user;
+        // console.log("USER::: ", req.user);
+
         next();
+
+    }catch(err){
+        return res.status(500).send("ERROR from userAuth Middleware: " + err);
     }
 }
-
-
-const userAuth = (req , res , next) =>{
-    console.log("User auth checked.")
-    const token = "sksksk";
-    if(token !== "sksksk"){
-        res.send("User is unAuthorised");
-    }else{
-        next();
-    }
-}
-module.exports = {adminAuth, userAuth}
+module.exports = {userAuth}
